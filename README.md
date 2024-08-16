98054cd116e6f88e92f485087f93d7d24cb3e002
ceoDemitri <demichole@gmail.com>
2024-08-16 02:26:46 -0500

    first commit. having a blast building with flutter. intro page structure complete

---
+++ README.md
@@ -0,0 +1,19 @@
+# sneakertutorial
+
+Learning with Mitch Koko
+https://www.youtube.com/watch?v=UcwsuZP071Y
+
+## Shoe Show Mobile Application
+
+8 16
+
+1. Completed Intro Page Build with action button to Naviagte to Home
+
+### FLutter / Dart
+
+1. import 'package:flutter/material.app';
+2. Wrapping
+3. How to Import images using pubspec.yaml
+4. Building an actionable splash screen. How to structure a page
+
+STOOPED VIDEO AT: 4:52 / 30:00 min. duration: 45min
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:sneakertutorial/pages/home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/logo.png',
+                  height: 200,
+                ),
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+
+              // title
+              const Text(
+                'Built Different',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 24),
+
+              // subtitle
+              const Text(
+                'How to fly without wings. A modern manual.',
+                style: TextStyle(fontSize: 16, color: Colors.black38),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+
+              // start now button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black,
+                  ),
+                  padding: EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'All Releases',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
